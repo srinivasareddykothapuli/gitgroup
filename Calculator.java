95289c6318b59db5938f1bbef73bafa485a3a0b8
Sandeep <2414364@cognizant.com>
2025-06-05 12:38:24 +0530

    Final Commit

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package com.java.calc;
+
+public class Calculator {
+	
+	public int getAddition(int a, int b) {
+		return a + b;
+	}
+	
+	public int getSubtraction(int a, int b) {
+		return a - b;
+	}
+	
+	public int getMultiplication(int a, int b) {
+		return a * b;
+	}
+	
+	public int getDivision(int a, int b) {
+		return a / b;
+	}
+	public int getModule(int a, int b) {             
+		return a  % b;
+	}
+
+}
